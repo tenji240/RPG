3780aa4553b202c62965830f91ef4ab52416e1ea
Tenji Tembo <tenji240@gmail.com>
2020-04-29 20:11:10 -0400

    #6 - added battle scene boilerplate

---
+++ phaser/src/scripts/scenes/battleScene.js
@@ -0,0 +1,17 @@
+export default class BattleScene extends Phaser.Scene {
+  constructor() {
+    super({key: 'BattleScene'});
+  }
+
+  preload() {
+    // ** load images here
+  }
+
+  create() {
+    // ** create the level here
+  }
+
+  update() {
+    // ** read button clicks and act accordingly
+  }
+}
